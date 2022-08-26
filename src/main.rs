168cc8fd960113c4392b7b1f540fda990f7132c5
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2022-08-26 21:03:12 +0100

    Adjustment of bot token placeholder

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn discord_request(endpoint:&str) -> String {
     let client = reqwest::Client::new();
     let req = client.get(String::from("https://discord.com/api/") + endpoint)
 
-    .header("Authorization", "Bot {}")
+    .header("Authorization", "Bot {TOKEN}")
     .header("User-Agent", "DiscordBot (https://harveycoombs.com/; 1.0.0.)")
     .header("Content-Type", "application/json; charset=UTF-8")
 

9f2e0dc2242288a1ef6c1e8a1f6b74266a9e6880
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-04-03 18:49:59 +0100

    bot instance structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
-// to-do
+use coral_tools;
+
+mod coral {
+    pub struct DiscordStatus {
+        text: String,
+        emoji_name: String,
+        emoji_id: u64
+    }
+
+    pub struct DiscordBot {
+        token: String,
+        status: DiscordStatus
+    }
+
+    impl DiscordBot {
+        pub fn create(token: String) -> self {
+            self.token = token;
+
+            let response = coral_tools::get_request("https://discord.com/api/gateway/bot").await?;
+            let response_body = response.text().await?;
+        }
+
+        pub fn set_status(status: &str) {
+            self.status = status.to_string();
+        }
+    }
+}

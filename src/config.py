21e097bfad0c78be4893376d1a65d0922d9d4390
harveycoombs <harveycoombs23@outlook.com>
2024-04-03 13:55:42 +0100

    Coral - Initial

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-class BotConfig:
-    token = ""
-    openai_token = ""
-    bot_id = 0
-    guild_id = 0
-    log_channel_id = 0
-    embed_color = discord.Color.from_rgb(0, 0, 0)
